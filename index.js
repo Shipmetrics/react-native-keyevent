fb9f973f9e200b12db742f55147ea1963c3db2fe
Kamal Mahyuddin <kamal@envoy.com>
2018-01-05 11:47:09 -0800

    Export eventEmitter so that we can test easier

--- index.js
+++ index.js
@@ -2,53 +2,54 @@ import {
   DeviceEventEmitter,
   NativeEventEmitter,
   NativeModules,
-  Platform
-} from "react-native";
-const { RNKeyEvent } = NativeModules;
+  Platform,
+} from 'react-native'
+const { RNKeyEvent } = NativeModules
 
 class KeyEvent {
-  constructor() {
-    this.emitter =
-      Platform.OS === "ios"
-        ? new NativeEventEmitter(RNKeyEvent)
-        : DeviceEventEmitter;
+  constructor(emitter) {
+    this.emitter = emitter
   }
 
   onKeyDownListener(cb) {
-    this.removeKeyDownListener();
-    this.listenerKeyDown = this.emitter.addListener("onKeyDown", cb);
+    this.removeKeyDownListener()
+    this.listenerKeyDown = this.emitter.addListener('onKeyDown', cb)
   }
 
   removeKeyDownListener() {
     if (this.listenerKeyDown) {
-      this.listenerKeyDown.remove();
-      this.listenerKeyDown = null;
+      this.listenerKeyDown.remove()
+      this.listenerKeyDown = null
     }
   }
 
   onKeyUpListener(cb) {
-    this.removeKeyUpListener();
-    this.listenerKeyUp = this.emitter.addListener("onKeyUp", cb);
+    this.removeKeyUpListener()
+    this.listenerKeyUp = this.emitter.addListener('onKeyUp', cb)
   }
 
   removeKeyUpListener() {
     if (this.listenerKeyUp) {
-      this.listenerKeyUp.remove();
-      this.listenerKeyUp = null;
+      this.listenerKeyUp.remove()
+      this.listenerKeyUp = null
     }
   }
 
   onKeyMultipleListener(cb) {
-    this.removeKeyMultipleListener();
-    this.listenerKeyMultiple = this.emitter.addListener("onKeyMultiple", cb);
+    this.removeKeyMultipleListener()
+    this.listenerKeyMultiple = this.emitter.addListener('onKeyMultiple', cb)
   }
 
   removeKeyMultipleListener() {
     if (this.listenerKeyMultiple) {
-      this.listenerKeyMultiple.remove();
-      this.listenerKeyMultiple = null;
+      this.listenerKeyMultiple.remove()
+      this.listenerKeyMultiple = null
     }
   }
 }
 
-export default new KeyEvent();
+export const eventEmitter =
+  Platform.OS === 'ios'
+    ? new NativeEventEmitter(RNKeyEvent)
+    : DeviceEventEmitter
+export default new KeyEvent(eventEmitter)
